b16001339f4e215467aed27cf288bf184ac96853
dongambas <ctmorris@gmail.com>
2018-05-26 00:56:46 -0300

    using zeppelin os

--- contracts/Toilet.sol
+++ contracts/Toilet.sol
@@ -1,12 +1,16 @@
 pragma solidity ^0.4.23;
 
- Transfer(address indexed from, address indexed to, uint256 value);
-}
-
+import "zos-lib/contracts/migrations/Migratable.sol";
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Basic.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol';
 
-contract Toilet{
+contract Toilet is Migratable{
+
+  uint256 public x;
+
+  function initialize(uint256 _x) isInitializer("Toilet", "0") public {
+    x = _x;
+  }
 
   /**
   * @dev This function can be called to transfer an ERC721 token to this contract address
@@ -21,7 +25,7 @@ contract Toilet{
   */
   function transferERC20(address fungibleAddress, uint256 value) internal {
       ERC20Basic fungibleToken = ERC20Basic(fungibleAddress);
-      fungibleToken.transfer(address(this), value;
+      fungibleToken.transfer(address(this), value);
   }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-var SimpleStorage = artifacts.require("SimpleStorage");
-var TutorialToken = artifacts.require("TutorialToken");
-var ComplexStorage = artifacts.require("ComplexStorage");
+var Toilet = artifacts.require("Toilet");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+  deployer.deploy(Toilet);
 };

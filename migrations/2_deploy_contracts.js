bc0c3ec69648059fd2299dabfa9ad862b17d1a45
Martin <martinnagelberg@gmail.com>
2018-05-27 09:40:01 -0300

    constructr and bug fixes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Toilet = artifacts.require("Toilet");
 
 module.exports = function(deployer) {
-  deployer.deploy(Toilet);
+  deployer.deploy(Toilet, "0xDb6d458D9f5aF7fDBE41748c7FBF862543556985");
 };
